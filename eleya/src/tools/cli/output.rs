5abcc68fcc3cb49d82106f6b4c3608959ac1290b
RiSer <sergiy19981@gmail.com>
2019-11-02 23:50:22 +0100

    add info debug function

--- eleya/src/tools/cli/output.rs
+++ eleya/src/tools/cli/output.rs
@@ -1,38 +1,35 @@
 use std::iter::FromIterator;
 use crate::tools::cli::vt100::escapes as ESC;
 
-// = status(ESC::LIGHT_RED, "error", ESC::RESET);
-////static ErrorType: &'static str = format!("({}{}{})",   ESC::LIGHT_RED, "error", ESC::RESET).as_str();
-//static InfoType: String = format!("({}{}{})",    ESC::LIGHT_BLUE, "info", ESC::RESET);
-//static WarningType: String = format!("({}{}{})", ESC::LIGHT_YELLOW, "warn", ESC::RESET);
-
-
-fn GenerateStatus<'b, 'a>(a: &'b str, b: &'a str, c: &'b str) -> String{
+fn generate_status<'b, 'a>(a: &'b str, b: &'a str, c: &'b str) -> String{
     format!("({}{}{})", a, b, c)
 }
 
-fn line_and_column_output(l: u32, c: u16) -> String {
+fn line_and_column_output(l: usize, c: usize) -> String {
     format!("({}{}{},{}{}{})",
             ESC::BOLD, l, ESC::RESET,
             ESC::BOLD, c, ESC::RESET
     )
 }
 
-pub fn error(line: &str, error_header: &str,  error_msg: &str, line_number: u32, column_number: u16) {
+pub fn error(line: &str, error_header: &str,  error_msg: &str, line_number: usize, column_number: usize) {
     println!("{} {} in line {} :\n"
-             , GenerateStatus(ESC::RED, "error", ESC::RESET)
+             , generate_status(ESC::RED, "error", ESC::RESET)
              , error_header
              , line_and_column_output(line_number, 1+column_number));
     let offset = (0..column_number).map(|_| " ").collect::<String>();
-//    println!("{} {} in line {} :", ErrorType, error_msg, line_and_column_output(line_number, column_number));
     println!("{}", line);
     println!("{}^---- {}", offset, error_msg);
     println!();
 }
 
+pub fn info(error_msg: &str) {
+    println!("{} {}", generate_status(ESC::BOLD, "info", ESC::RESET), error_msg);
+}
+
 pub fn debug<'a>(msg: &'a str, line: &'a str){
     println!("{} {} : {}\n"
-             , GenerateStatus(ESC::GREEN, "debug", ESC::RESET)
+             , generate_status(ESC::GREEN, "debug", ESC::RESET)
              , msg
              , line);
 }
