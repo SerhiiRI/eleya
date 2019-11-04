cd6c6c860ff32d99f8bad5e0e59eae2efa70db46
Serhii Riznychuk <sergiy19981@gmail.com>
2019-11-04 15:20:24 +0100

    Create tests.rs

---
+++ eleya/src/tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn the_line_is_comment(){}
+    
+    #[test]
+    fn the_line_is_parameter(){}
+    
+    #[test]
+    fn the_line_is_frame(){}
+    
+    #[test]
+    fn parsing_parameter_line(){}
+    
+    #[test]
+    fn validate_value_parameter(){}
+}
