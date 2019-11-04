8070c5ae0238a0ab18945a11c7a14c852e62e3e6
RiSer <sergiy19981@gmail.com>
2019-11-04 02:36:42 +0100

    add [dead_code] to debug output methods

--- eleya/src/tools/cli/output.rs
+++ eleya/src/tools/cli/output.rs
@@ -1,17 +1,18 @@
-use std::iter::FromIterator;
 use crate::tools::cli::vt100::escapes as ESC;
 
+
+#[allow(dead_code)]
 fn generate_status<'b, 'a>(a: &'b str, b: &'a str, c: &'b str) -> String{
     format!("({}{}{})", a, b, c)
 }
-
+#[allow(dead_code)]
 fn line_and_column_output(l: usize, c: usize) -> String {
     format!("({}{}{},{}{}{})",
             ESC::BOLD, l, ESC::RESET,
             ESC::BOLD, c, ESC::RESET
     )
 }
-
+#[allow(dead_code)]
 pub fn error(line: &str, error_header: &str,  error_msg: &str, line_number: usize, column_number: usize) {
     println!("{} {} in line {} :\n"
              , generate_status(ESC::RED, "error", ESC::RESET)
@@ -22,18 +23,23 @@ pub fn error(line: &str, error_header: &str,  error_msg: &str, line_number: usiz
     println!("{}^---- {}", offset, error_msg);
     println!();
 }
-
+#[allow(dead_code)]
+pub fn error_line(error_msg: &str) {
+    println!("{} {}", generate_status(ESC::RED, "error", ESC::RESET), error_msg);
+}
+#[allow(dead_code)]
 pub fn info(error_msg: &str) {
     println!("{} {}", generate_status(ESC::BOLD, "info", ESC::RESET), error_msg);
 }
 
+#[allow(dead_code)]
 pub fn debug<'a>(msg: &'a str, line: &'a str){
     println!("{} {} : {}\n"
              , generate_status(ESC::GREEN, "debug", ESC::RESET)
              , msg
              , line);
 }
-
+#[allow(dead_code)]
 pub fn comment(line: &str){
     println!("{}{}{}", ESC::DARK_GRAY, line, ESC::RESET);
 }
