ec8263ef24e839f46dcb09e4b2ea3ad9b779d73e
Serhii Riznychuk <sergiy19981@gmail.com>
2019-11-13 14:43:04 +0100

    add escape expands

--- eleya/src/tools/cli/vt100.rs
+++ eleya/src/tools/cli/vt100.rs
@@ -37,10 +37,10 @@ pub mod escapes {
     pub const CURSOR_NEXT_LINE:&str = "\x1B[{}E";
     pub const CURSOR_PREV_LINE:&str = "\x1B[{}F";
     pub const CURSOR_HORIZONTAL_ABSOLUTE:&str = "\x1B[{}G"; //moved horizontal in current line
-    pub const CURSOR_VERTICAL_ABSOLUTE:&str = "\x1B[{}d"; //moved verticaly in current columnt
+    pub const CURSOR_VERTICAL_ABSOLUTE:  &str = "\x1B[{}d"; //moved verticaly in current columnt
     
-    pub const CURSOR_ENABLE_BLINCKING:&str = "\x1B[12h";
-    pub const CURSOR_DISABLE_BLINCKING:&str = "\x1B[12l";
+    pub const CURSOR_ENABLE_BLINCKING:  &str = "\x1B[12h";
+    pub const CURSOR_DISABLE_BLINCKING: &str = "\x1B[12l";
     pub const CURSOR_SHOW:&str = "\x1B[25h";
     pub const CURSOR_HIDE:&str = "\x1B[25l";
     
@@ -48,28 +48,28 @@ pub mod escapes {
     // x1B[ 1 J: erase from start to cursor.
     // x1B[ 2 J: erase whole display.
     // x1B[ 3 J: erase whole display including scroll-back
-    pub const ERASE_DISPLAY_FROM_START_TO_CURSOR:    &str = "\x1B[1J";
-    pub const ERASE_DISPLAY:    &str = "\x1B[2J";
-    pub const ERASE_DISPLAY_WITH_SCROLL:    &str = "\x1B[3J";
+    pub const ERASE_DISPLAY_FROM_START_TO_CURSOR:  &str = "\x1B[1J";
+    pub const ERASE_DISPLAY:                       &str = "\x1B[2J";
+    pub const ERASE_DISPLAY_WITH_SCROLL:           &str = "\x1B[3J";
   
     // x1B[ K: Erase line (default: from cursor to end of line).
     // x1B[ 1 K: erase from start of line to cursor.
     // x1B[ 2 K: erase whole line.
-    pub const ERASE_LINE_FROM_CURSOR_TO_END: &str = "\x1B[K";
-    pub const ERASE_LINE_FROM_START_TO_CURSOR: &str = "\x1B[1K";
-    pub const ERASE_LINE: &str = "\x1B[2K";
+    pub const ERASE_LINE_FROM_CURSOR_TO_END:       &str = "\x1B[K";
+    pub const ERASE_LINE_FROM_START_TO_CURSOR:     &str = "\x1B[1K";
+    pub const ERASE_LINE:                          &str = "\x1B[2K";
     
-    pub const INSERT_CHARACTER: &str = "\x1B[{}@"; // Insert {} spaces at the current cursor position, shifting all existing text to the right. Text exiting the screen to the right is removed.
-    pub const DELETE_CHARACTER: &str = "\x1B[{}P"; // Delete {} characters at the current cursor position, shifting in space characters from the right edge of the screen.
-    pub const ERASE_CHARACTER: &str = "\x1B[{}X"; // Erase {} characters from the current cursor position by overwriting them with a space character.
-    pub const INSERT_LINES: &str = "\x1B[{}L"; // Inserts {} lines into the buffer at the cursor position. The line the cursor is on, and lines below it, will be shifted downwards
-    pub const DELETE_LINES: &str = "\x1B[{}M"; // Deletes {} lines from the buffer, starting with the row the cursor is on.
+    pub const INSERT_CHARACTER:  &str = "\x1B[{}@"; // Insert {} spaces at the current cursor position, shifting all existing text to the right. Text exiting the screen to the right is removed.
+    pub const DELETE_CHARACTER:  &str = "\x1B[{}P"; // Delete {} characters at the current cursor position, shifting in space characters from the right edge of the screen.
+    pub const ERASE_CHARACTER:   &str = "\x1B[{}X"; // Erase {} characters from the current cursor position by overwriting them with a space character.
+    pub const INSERT_LINES:      &str = "\x1B[{}L"; // Inserts {} lines into the buffer at the cursor position. The line the cursor is on, and lines below it, will be shifted downwards
+    pub const DELETE_LINES:      &str = "\x1B[{}M"; // Deletes {} lines from the buffer, starting with the row the cursor is on.
     
     pub const SOFT_RESET_TERMINAL_SETTINGS: &str = "\x1B[!p";
     
     
     // DEC Line drawing mode
-    pub const DEC_MODE_ENABLE: &str = "\x(0";
+    pub const DEC_MODE_ENABLE:  &str = "\x(0";
     pub const DEC_MODE_DISABLE: &str = "\x(B";
     // box charset
     pub const DEC_6a: &str = "0x6a"; // ┘
@@ -83,6 +83,19 @@ pub mod escapes {
     pub const DEC_76: &str = "0x76"; // ┴
     pub const DEC_77: &str = "0x77"; // ┬
     pub const DEC_78: &str = "0x78"; // │
-    
-    
+    //┌─────────────┐
+    //│             │
+    //│  for        │
+    //│   drowing   │
+    //│     boxes   │
+    //│             │
+    //└─────────────┘
 }
+
+pub mod escapes_hint{
+    macro_rules! escape {
+	() => ("".to_string());
+	($fmt:ty, $($arg:expr)* ) => ({format!($fmt, $($arg)*)});
+    }
+}
+
